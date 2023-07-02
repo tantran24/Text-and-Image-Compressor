dfbde8c3adb5f5c15db2be830d5b815d6dbe08cf
Nguyen Duy Thai <92375607+Tox1cCoder@users.noreply.github.com>
2023-07-02 20:18:39 +0700

    Add files via upload

--- apps/algorithms/adaptive_huffman.py
+++ apps/algorithms/adaptive_huffman.py
@@ -1,233 +1,194 @@
-import operator
-
-from bitarray import bitarray, bits2bytes
-
-from .utils.tree import Tree, NYT, exchange
-from .utils.utils import (encode_dpcm, decode_dpcm, bin_str2bool_list, bool_list2int)
+from PIL import Image
+import os
+import numpy as np
 
 
-class AdaptiveHuffman:
-    def __init__(self, byte_seq, alphabet_range=(0, 255), dpcm=False):
-        """Create an adaptive huffman encoder and decoder.
+class AdaptiveHuffman_IMG:
+    def __init__(self, path):
+        self.path = path
+        self.original_file_size = os.path.getsize(path)
 
-        Args:
-            byte_seq (bytes): The bytes sequence to encode or decode.
-            alphabet_range (tuple or integer): The range of alphabet
-                inclusively.
-        """
+    def compress(self):
+        self.initCompress()
+        compressedColors = []
+        print("Compressing Image ...")
+        compressedColors.append(self.compressColor(self.red))
+        print("Compressing Image ...")
+        compressedColors.append(self.compressColor(self.green))
+        print("Compressing Image ...")
+        compressedColors.append(self.compressColor(self.blue))
+        print("Image Compressed --------- Writing to File")
+        filesplit = os.path.basename(self.path).split('.')
+        filename = filesplit[0] + '_AdaptiveHuffmanCompressed.txt'
+        savingDirectory = os.path.join(os.getcwd(), 'CompressedFiles')
+        if not os.path.isdir(savingDirectory):
+            os.makedirs(savingDirectory)
+        with open(os.path.join(savingDirectory, filename), 'w') as file:
+            for color in compressedColors:
+                for row in color:
+                    file.write(row)
+                    file.write("\n")
+        self.compressed_file_size = os.path.getsize(os.path.join(savingDirectory, filename))
 
-        self.byte_seq = byte_seq
-        self.dpcm = dpcm
+    def compressColor(self, colorList):
+        compressedColor = []
+        for currentRow in colorList:
+            compressedRow = ""
+            freq_dict = {}
+            huff_tree = HuffmanTree()
+            for char in currentRow:
+                if char not in freq_dict:
+                    freq_dict[char] = 1
+                else:
+                    freq_dict[char] += 1
+                compressedChar = huff_tree.encode(char)
+                compressedRow += compressedChar
+            compressedColor.append(compressedRow)
+        return compressedColor
 
-        self._bits = None  # Only used in decode().
-        self._bits_idx = 0  # Only used in decode().
+    def decompress(self):
+        print("Decompressing File ...")
+        image = []
+        with open(self.path, "r") as file:
+            for line in file:
+                decodedRow = self.decompressRow(line)
+                image.append(np.array(decodedRow))
+        image = np.array(image)
+        shapeTup = image.shape
+        image = image.reshape((3, shapeTup[0] // 3, shapeTup[1]))
+        self.saveImage(image)
+        print("Decompression Done.")
 
-        # Get the first decimal number of all alphabets
-        self._alphabet_first_num = min(alphabet_range)
-        alphabet_size = abs(alphabet_range[0] - alphabet_range[1]) + 1
-        # Select an `exp` and `rem` which meet `alphabet_size = 2**exp + rem`.
-        # Get the largest `exp` smaller than `alphabet_size`.
-        self.exp = alphabet_size.bit_length() - 1
-        self.rem = alphabet_size - 2 ** self.exp
+    def decompressRow(self, line):
+        currentRow = line.split(",")
+        currentRow[-1] = currentRow[-1][:-1]
+        decodedRow = ""
+        huff_tree = HuffmanTree()
+        for code in currentRow:
+            decodedChar = huff_tree.decode(code)
+            decodedRow += decodedChar
+            huff_tree.update(decodedChar)
+        newRow = decodedRow.split(',')
+        decodedRow = [int(x) for x in newRow]
+        return decodedRow
 
-        # Initialize the current node # as the maximum number of nodes with
-        # `alphabet_size` leaves in a complete binary tree.
-        self.current_node_num = alphabet_size * 2 - 1
+    def initCompress(self):
+        self.image = Image.open(self.path)
+        self.height, self.width = self.image.size
+        self.red, self.green, self.blue = self.processImage()
 
-        self.tree = Tree(0, self.current_node_num, data=NYT)
-        self.all_nodes = [self.tree]
-        self.nyt = self.tree  # initialize the NYT reference
+    def processImage(self):
+        image = self.image.convert('RGB')
+        red, green, blue = [], [], []
+        pixel_values = list(image.getdata())
+        iterator = 0
+        for height_index in range(self.height):
+            R, G, B = "", "", ""
+            for width_index in range(self.width):
+                RGB = pixel_values[iterator]
+                R = R + str(RGB[0]) + ","
+                G = G + str(RGB[1]) + ","
+                B = B + str(RGB[2]) + ","
+                iterator += 1
+            red.append(R[:-1])
+            green.append(G[:-1])
+            blue.append(B[:-1])
+        return red, green, blue
 
-    def encode(self):
-        """Encode the target byte sequence into compressed bit sequence by
-        adaptive Huffman coding.
+    def saveImage(self, image):
+        print("Saving Decompressed File...")
+        filesplit = os.path.basename(self.path).split('_AdaptiveHuffmanCompressed.txt')
+        filename = filesplit[0] + "_AdaptiveHuffmanDecompressed.jpg"
+        savingDirectory = os.path.join(os.getcwd(), 'DecompressedFiles')
+        if not os.path.isdir(savingDirectory):
+            os.makedirs(savingDirectory)
+        imagelist, imagesize = self.makeImageData(image[0], image[1], image[2])
+        imagenew = Image.new('RGB', imagesize)
+        imagenew.putdata(imagelist)
+        imagenew.save(os.path.join(savingDirectory, filename))
 
-        Returns:
-            bitarray: The compressed bitarray. Use `bitarray.tofile()` to save
-                to file.
-        """
+    def makeImageData(self, r, g, b):
+        imagelist = []
+        for i in range(len(r)):
+            for j in range(len(r[0])):
+                imagelist.append((r[i][j], g[i][j], b[i][j]))
+        return imagelist, (len(r), len(r[0]))
 
-        def encode_fixed_code(dec):
-            """Convert a decimal number into specified fixed code.
 
-            Arguments:
-                dec {int} -- The alphabet need to be converted into fixed code.
+class HuffmanNode:
+    def __init__(self, char, frequency):
+        self.char = char
+        self.frequency = frequency
+        self.left = None
+        self.right = None
 
-            Returns:
-                list of bool -- Fixed codes.
-            """
 
-            alphabet_idx = dec - (self._alphabet_first_num - 1)
-            if alphabet_idx <= 2 * self.rem:
-                fixed_str = '{:0{padding}b}'.format(
-                    alphabet_idx - 1,
-                    padding=self.exp + 1
-                )
+class HuffmanTree:
+    def __init__(self):
+        self.root = HuffmanNode(None, 0)
+        self.dictionary = {}
+
+    def encode(self, char):
+        if char not in self.dictionary:
+            code = self.generate_code(char)
+            self.dictionary[char] = code
+            return code
+        else:
+            return self.dictionary[char]
+
+    def generate_code(self, char):
+        node = self.root
+        code = ""
+        while node.char is not None:
+            if char in node.left.char:
+                code += "0"
+                node = node.left
+            elif char in node.right.char:
+                code += "1"
+                node = node.right
+        return code
+
+    def decode(self, code):
+        node = self.root
+        for bit in code:
+            if bit == "0":
+                node = node.left
+            elif bit == "1":
+                node = node.right
+        return node.char
+
+    def update(self, char):
+        node = self.root
+        while node.char is not None:
+            if char in node.left.char:
+                node = node.left
+            elif char in node.right.char:
+                node = node.right
+        new_node = HuffmanNode(char, 1)
+        left_node = HuffmanNode(node.char, node.frequency)
+        node.char = None
+        node.frequency += 1
+        node.left = left_node
+        node.right = new_node
+
+        if left_node.frequency > new_node.frequency:
+            node.left, node.right = node.right, node.left
+        self.rebalance(node)
+
+    def rebalance(self, node):
+        while node.char is None:
+            left_frequency = node.left.frequency if node.left is not None else float("inf")
+            right_frequency = node.right.frequency if node.right is not None else float("inf")
+            min_child = node.left if left_frequency <= right_frequency else node.right
+            if min_child.frequency < node.frequency:
+                node.left, node.right = node.right, node.left
+                node.char = min_child.char
+                min_child.char = None
+                min_child.frequency = 0
+                node.frequency += 1
+                node = min_child
             else:
-                fixed_str = '{:0{padding}b}'.format(
-                    alphabet_idx - self.rem - 1,
-                    padding=self.exp
-                )
-            return bin_str2bool_list(fixed_str)
-
-        if self.dpcm:
-            self.byte_seq = tuple(encode_dpcm(self.byte_seq))
-
-        code = []
-        for symbol in self.byte_seq:
-            fixed_code = encode_fixed_code(symbol)
-            result = self.tree.search(fixed_code)
-            if result['first_appearance']:
-                code.extend(result['code'])  # send code of NYT
-                code.extend(fixed_code)  # send fixed code of symbol
-            else:
-                # send code which is path from root to the node of symbol
-                code.extend(result['code'])
-            self.update(fixed_code, result['first_appearance'])
-
-        # Add remaining bits length info at the beginning of the code in order
-        # to avoid the decoder regarding the remaining bits as actual data. The
-        # remaining bits length info require 3 bits to store the length. Note
-        # that the first 3 bits are stored as big endian binary string.
-        remaining_bits_length = (
-                bits2bytes(len(code) + 3) * 8 - (len(code) + 3)
-        )
-        code = (bin_str2bool_list('{:03b}'.format(remaining_bits_length))
-                + code)
-
-        return bitarray(code)
-
-    def decode(self):
-        """Decode the target byte sequence which is encoded by adaptive Huffman
-        coding.
-
-        Returns:
-            list: A list of integer representing the number of decoded byte
-                sequence.
-        """
-
-        def read_bits(bit_count):
-            """Read n leftmost bits and move iterator n steps.
-
-            Arguments:
-                n {int} -- The # of bits is about to read.
-
-            Returns:
-                list -- The n bits has been read.
-            """
-
-            ret = self._bits[self._bits_idx:self._bits_idx + bit_count]
-            self._bits_idx += bit_count
-            return ret
-
-        def decode_fixed_code():
-            fixed_code = read_bits(self.exp)
-            integer = bool_list2int(fixed_code)
-            if integer < self.rem:
-                fixed_code += read_bits(1)
-                integer = bool_list2int(fixed_code)
-            else:
-                integer += self.rem
-            return integer + 1 + (self._alphabet_first_num - 1)
-
-        # Get boolean list ([True, False, ...]) from bytes.
-        bits = bitarray()
-        bits.frombytes(self.byte_seq)
-        self._bits = bits.tolist()
-        self._bits_idx = 0
-
-        # Remove the remaining bits in the last of bit sequence generated by
-        # bitarray.tofile() to fill up to complete byte size (8 bits). The
-        # remaining bits length could be retrieved by reading the first 3 bits.
-        # Note that the first 3 bits are stored as big endian binary string.
-        remaining_bits_length = bool_list2int(read_bits(3))
-        if remaining_bits_length:
-            del self._bits[-remaining_bits_length:]
-        self._bits = tuple(self._bits)
-
-        code = []
-        while self._bits_idx < len(self._bits):
-            current_node = self.tree  # go to root
-            while current_node.left or current_node.right:
-                bit = read_bits(1)[0]
-                current_node = current_node.right if bit else current_node.left
-            if current_node.data == NYT:
-                first_appearance = True
-                dec = decode_fixed_code()
-                code.append(dec)
-            else:
-                # decode element corresponding to node
-                first_appearance = False
-                dec = current_node.data
-                code.append(current_node.data)
-            self.update(dec, first_appearance)
-
-        return decode_dpcm(code) if self.dpcm else code
-
-    def update(self, data, first_appearance):
-
-        def find_node_data(data):
-            for node in self.all_nodes:
-                if node.data == data:
-                    return node
-            raise KeyError(f'Cannot find the target node given {data}.')
-
-        current_node = None
-        while True:
-            if first_appearance:
-                current_node = self.nyt
-
-                self.current_node_num -= 1
-                new_external = Tree(1, self.current_node_num, data=data)
-                current_node.right = new_external
-                self.all_nodes.append(new_external)
-
-                self.current_node_num -= 1
-                self.nyt = Tree(0, self.current_node_num, data=NYT)
-                current_node.left = self.nyt
-                self.all_nodes.append(self.nyt)
-
-                current_node.weight += 1
-                current_node.data = None
-                self.nyt = current_node.left
-            else:
-                if not current_node:
-                    # First time as `current_node` is None.
-                    current_node = find_node_data(data)
-                node_max_num_in_block = max(
-                    (
-                        n for n in self.all_nodes
-                        if n.weight == current_node.weight
-                    ),
-                    key=operator.attrgetter('num')
-                )
-                if node_max_num_in_block not in (current_node, current_node.parent):
-                    exchange(current_node, node_max_num_in_block)
-                    current_node = node_max_num_in_block
-                current_node.weight += 1
-            if not current_node.parent:
-                break
-            current_node = current_node.parent
-            first_appearance = False
-
-
-def compress(in_filename, out_filename, alphabet_range, dpcm):
-    with open(in_filename, 'rb') as in_file:
-        content = in_file.read()
-
-    ada_huff = AdaptiveHuffman(content, alphabet_range, dpcm)
-    code = ada_huff.encode()
-
-    with open(out_filename, 'wb') as out_file:
-        code.tofile(out_file)
-
-
-def extract(in_filename, out_filename, alphabet_range, dpcm):
-    with open(in_filename, 'rb') as in_file:
-        content = in_file.read()
-    ada_huff = AdaptiveHuffman(content, alphabet_range, dpcm)
-    code = ada_huff.decode()
-
-    with open(out_filename, 'wb') as out_file:
-        out_file.write(bytes(code))
-
+                node.frequency += 1
+                node = node.parent
+                if node is None:
+                    break
--- apps/image_compression.py
+++ apps/image_compression.py
@@ -1,66 +1,102 @@
-import cv2
-import numpy as np
+import os
 import streamlit as st
 from PIL import Image
-import os
 from .algorithms import adaptive_huffman, lzw
-from .algorithms.utils.utils import get_raw_img
+
+def compress_images(uploaded_files, compression_algorithm):
+    compressed_files = []
+
+    for uploaded_file in uploaded_files:
+        image = Image.open(uploaded_file)
+        image_path = f"temp_images/{uploaded_file.name}"
+        image.save(image_path)
+
+        if compression_algorithm == 'Adaptive Huffman':
+            compressor = adaptive_huffman.AdaptiveHuffman_IMG(image_path)
+            compressor.compress()
+            compressed_file_path = f"CompressedFiles/{uploaded_file.name.split('.')[0]}_AdaptiveHuffmanCompressed.txt"
+            compressed_files.append(compressed_file_path)
+
+        elif compression_algorithm == 'LZW':
+            compressor = lzw.LZW_IMG(image_path)
+            compressor.compress()
+            compressed_file_path = f"CompressedFiles/{uploaded_file.name.split('.')[0]}_LZWCompressed.txt"
+            compressed_files.append(compressed_file_path)
+
+    return compressed_files
 
 
-def load_image(img):
-    im = Image.open(img)
-    image = np.array(im)
-    return image
+def decompress_images(compressed_files, decompression_algorithm):
+    decompressed_images = []
 
+    for compressed_file in compressed_files:
+        decompressed_file_path = ""
 
-def get_file_size(file_path):
-    with open(file_path, 'rb') as file:
-        return len(file.read())
+        if decompression_algorithm == 'Adaptive Huffman':
+            decompressed_file_path = f"DecompressedFiles/{os.path.basename(compressed_file).split('_AdaptiveHuffmanCompressed.txt')[0]}_AdaptiveHuffmanDecompressed.jpg"
+            compressor = adaptive_huffman.AdaptiveHuffman_IMG(compressed_file)
+
+        elif decompression_algorithm == 'LZW':
+            decompressed_file_path = f"DecompressedFiles/{os.path.basename(compressed_file).split('_LZWCompressed.txt')[0]}_LZWDecompressed.jpg"
+            compressor = lzw.LZW_IMG(compressed_file)
+
+        compressor.decompress()
+        decompressed_image = Image.open(decompressed_file_path)
+        decompressed_images.append(decompressed_image)
+
+    return decompressed_images
 
 
 def image_compression():
     st.title("Image Compression")
 
-    st.markdown("**Upload up to 20 images**")
-    uploaded_files = st.file_uploader("Choose images", accept_multiple_files=True, type=['jpg', 'jpeg', 'png', 'raw'],
-                                      key="image")
+    mode_options = ['Compress', 'Decompress']
+    mode = st.selectbox("Select mode", mode_options)
 
-    compression_options = ['Adaptive Huffman', 'LZW', 'JPEG']
-    compression_algorithm = st.selectbox("Select compression algorithm", compression_options)
+    if mode == 'Compress':
+        st.markdown("**Upload up to 20 images**")
+        uploaded_files = st.file_uploader("Choose images", accept_multiple_files=True,
+                                          type=['jpg', 'jpeg', 'png'], key="image")
 
-    if st.button("Compress"):
-        if not uploaded_files:
-            st.warning("Please upload at least one image.")
-            return
+        compression_options = ['Adaptive Huffman', 'LZW']
+        compression_algorithm = st.selectbox("Select compression algorithm", compression_options)
 
-        for uploaded_file in uploaded_files:
-            image = Image.open(uploaded_file)
-            image_path = f"temp_images/{uploaded_file.name}"
-            image.save(image_path)
+        if st.button("Compress"):
+            if not uploaded_files:
+                st.warning("Please upload at least one image.")
+                return
 
-            if compression_algorithm == 'Adaptive Huffman':
-                ah = adaptive_huffman
-                alphabet_range = (0, 255)
-                dpcm = False
-                ah.compress(uploaded_file.name, 'compressed',
-                            alphabet_range=alphabet_range, dpcm=dpcm)
-                ah.extract('compressed', 'extracted.raw',
-                           alphabet_range=alphabet_range, dpcm=dpcm)
-
-                comparison = get_raw_img(uploaded_file.name, 'extracted.raw', size=(512, 512))
-
-                st.image(comparison)
-
-            elif compression_algorithm == 'LZW':
-                compressor = lzw.LZW_IMG(image_path)
-                compressor.compress()
-                before = compressor.original_file_size
-                after = compressor.compressed_file_size
-
-                decompressor = lzw.LZW_IMG(os.path.join("CompressedFiles",uploaded_file.name.split('.')[0] + "_LZWcompressed.txt"))
-                decompressor.decompress()
+            compressed_files = compress_images(uploaded_files, compression_algorithm)
 
             st.markdown("----")
+            st.success("Compression completed!")
+            st.markdown("**Download compressed files:**")
+            for compressed_file in compressed_files:
+                st.download_button(
+                    label="DOWNLOAD " + os.path.basename(compressed_file),
+                    data=compressed_file,
+                    file_name=os.path.basename(compressed_file)
+                )
+
+    elif mode == 'Decompress':
+        st.markdown("**Upload compressed files**")
+        compressed_files = st.file_uploader("Choose files", accept_multiple_files=True, type=['txt'], key="compressed_files")
+
+        decompression_options = ['Adaptive Huffman', 'LZW']
+        decompression_algorithm = st.selectbox("Select decompression algorithm", decompression_options)
+
+        if st.button("Decompress"):
+            if not compressed_files:
+                st.warning("Please upload at least one compressed file.")
+                return
+
+            decompressed_images = decompress_images(compressed_files, decompression_algorithm)
+
+            st.markdown("----")
+            st.success("Decompression completed!")
+            st.markdown("**Original Images:**")
+            for image in decompressed_images:
+                st.image(image)
 
 
 def run():
